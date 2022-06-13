61e36aff800eff8591fcacf81d91c716518a6e32
David ONeill <mrdavidoneillll@gmail.com>
2022-06-13 22:49:28 +0100

    Add Kit and KitType serializers

--- kithub/api/serializers.py
+++ kithub/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,15 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+
+class KitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kit
+        fields = "__all__"
+
+
+class KitTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KitType
+        fields = "__all__"
