8d50057bb1e58b7d4f1847be23851924eb1ec4e1
David ONeill <mrdavidoneillll@gmail.com>
2022-06-13 19:25:12 +0100

    Format using black

--- kithub/wsgi.py
+++ kithub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kithub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kithub.settings")
 
 application = get_wsgi_application()
