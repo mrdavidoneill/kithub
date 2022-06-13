7729fc0ac0e319091ef7f001336cf811bb35640a
David ONeill <mrdavidoneillll@gmail.com>
2022-06-13 22:48:23 +0100

    Refactor router into own file

--- kithub/urls.py
+++ kithub/urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from kithub.api import views
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
+from .router import router
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
