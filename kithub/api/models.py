6fb28b9f2c2cdbc137c1b34ee72284f0c6a8a5fd
David ONeill <mrdavidoneillll@gmail.com>
2022-06-13 19:25:06 +0100

    Add kit and kit type models

--- kithub/api/models.py
+++ kithub/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Kit(models.Model):
+    """Kit Model
+    Defines attributes of a DIY Kit"""
+
+    name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField(default=0)  # 0 to 2147483647
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class KitType(models.Model):
+    """Kit Type Model
+    Defines attributes of a DIY Kit type"""
+
+    kind = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
