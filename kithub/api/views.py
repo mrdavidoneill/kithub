4d8469eaeb1444d43f977584235db8c55ce2c818
David ONeill <mrdavidoneillll@gmail.com>
2022-06-13 22:49:55 +0100

    Add Kit and KitType viewsets

--- kithub/api/views.py
+++ kithub/api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from kithub.api.serializers import UserSerializer, GroupSerializer
+from .serializers import *
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +21,13 @@ class GroupViewSet(viewsets.ModelViewSet):
 
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class KitViewSet(viewsets.ModelViewSet):
+    queryset = Kit.objects.all()
+    serializer_class = KitSerializer
+
+
+class KitTypeViewSet(viewsets.ModelViewSet):
+    queryset = KitType.objects.all()
+    serializer_class = KitTypeSerializer
