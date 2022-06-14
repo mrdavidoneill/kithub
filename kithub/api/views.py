9d83979852b202cc479101ecdaae821b3ca12c51
David ONeill <mrdavidoneillll@gmail.com>
2022-06-14 17:39:03 +0100

    Remove user and group default code - no user data stored

--- kithub/api/views.py
+++ kithub/api/views.py
@@ -5,24 +5,6 @@ from kithub.api.serializers import UserSerializer, GroupSerializer
 from .serializers import *
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-
-    queryset = User.objects.all().order_by("-date_joined")
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
 class KitViewSet(viewsets.ModelViewSet):
     queryset = Kit.objects.all()
     serializer_class = KitSerializer
