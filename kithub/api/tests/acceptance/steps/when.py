3a0f565ab6f7005ed5d4b6e5000695be94fde5fb
David ONeill <mrdavidoneillll@gmail.com>
2022-06-20 15:58:52 +0100

    Fix bug with too long a name for parts/bags - should be less than 4 letters Add request partslist Add request potential kits

--- kithub/api/tests/acceptance/steps/when.py
+++ kithub/api/tests/acceptance/steps/when.py
@@ -1,3 +1,4 @@
+import string
 import json
 from behave import when
 from django.urls import reverse
@@ -16,6 +17,26 @@ def create_model(context, model, name):
     if model == "part":
 
         payload = {"name": name, "description": f"A description of {name}"}
+    else:
+        payload = {"kind": name}
+
+    context.response = context.test.client.post(reverse(f"{model}-list"), payload)
+
+
+@when(
+    'I create "{quantity:Number}" "{model}" of "{kind}" "{kindmodel}" with name "{name}"'
+)
+def create_model_with_name(context, quantity, model, kind, kindmodel, name):
+
+    kinds = context.test.client.get(reverse(f"{kindmodel}-list")).data
+
+    if model == "bag" or model == "kit":
+        payload = {
+            "name": name,
+            "quantity": int(quantity),
+            "complete": False,
+            "kind": get_item_by_key(kinds, kind, key="kind")["id"],
+        }
     else:
         payload = {"kind": name}
 
@@ -143,3 +164,55 @@ def delete_first_model(context, model):
     )
     print(context.response)
     print(context.response.data)
+
+
+@when('I request a partlist for "{quantity:Number}" of "{kittype}"')
+def request_partslist(context, quantity, kittype):
+    kittypes = context.test.client.get(reverse("kittype-list")).data
+    print(f"kittypes: {kittypes}")
+    kittype = get_item_by_key(kittypes, kittype, "kind")
+    print(f"kittype: {kittype}")
+    context.response = context.test.client.get(
+        reverse("partstobuyforkit", args=(kittype["id"], quantity))
+    )
+
+    print(context.response)
+    print(context.response.data)
+
+
+@when('I request potential kits of "{kittype}"')
+def request_potential_kits(context, kittype):
+    kittypes = context.test.client.get(reverse("kittype-list")).data
+    print(f"kittypes: {kittypes}")
+    kittype = get_item_by_key(kittypes, kittype, "kind")
+    print(f"kittype: {kittype}")
+    context.response = context.test.client.get(
+        reverse("potentialkits", args=(kittype["id"],))
+    )
+
+    print(context.response)
+    print(context.response.data)
+
+
+@when('I create a kit type "{name}" with bags list of')
+def create_specific_kittype(context, name):
+    kittype = context.test.client.post(
+        reverse("kittype-list"),
+        {"kind": name},
+    )
+    print(f"kittype.data {kittype.data}")
+
+    bagtypes = context.test.client.get(reverse("bagtype-list")).data
+
+    print(bagtypes)
+
+    for index, row in enumerate(context.table):
+        context.response = context.test.client.post(
+            reverse("kitingredient-list"),
+            {
+                "name": string.ascii_letters[index].upper(),
+                "kittype": kittype.data["id"],
+                "quantity": int(row["quantity"]),
+                "bagtype": get_item_by_key(bagtypes, row["bagtype"], key="kind")["id"],
+            },
+        )
