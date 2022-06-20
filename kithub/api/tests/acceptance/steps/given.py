7a94324cfdcbdebd25baacad30e66a2006ce8336
David ONeill <mrdavidoneillll@gmail.com>
2022-06-20 10:51:26 +0100

    Add create_set_of_bags Add create_specific_bagtype

--- kithub/api/tests/acceptance/steps/given.py
+++ kithub/api/tests/acceptance/steps/given.py
@@ -1,5 +1,6 @@
 from behave import given
 from django.urls import reverse
+from utils import get_part_by_name, get_bagtype_by_kind
 
 
 @given("this step exists")
@@ -27,6 +28,46 @@ def create_set_of_parts(context):
                 "name": row["name"],
                 "description": row["description"],
                 "quantity": row["quantity"],
+            },
+        )
+
+
+@given("a set of specific bags")
+def create_set_of_bags(context):
+    bagtypes = context.test.client.get(reverse("bagtype-list")).data
+
+    for row in context.table:
+        context.response = context.test.client.post(
+            reverse("bag-list"),
+            {
+                "name": row["name"],
+                "quantity": int(row["quantity"]),
+                "complete": bool(row["complete"]),
+                "kind": get_bagtype_by_kind(bagtypes, row["kind"])["id"],
+            },
+        )
+
+
+@given('a bag type "{name}" with parts list of')
+def create_specific_bagtype(context, name):
+    bagtype = context.test.client.post(
+        reverse("bagtype-list"),
+        {"kind": name},
+    )
+    print(f"bagtype.data {bagtype.data}")
+
+    parts = context.test.client.get(reverse("part-list")).data
+
+    print(parts)
+
+    for row in context.table:
+        context.response = context.test.client.post(
+            reverse("bagingredient-list"),
+            {
+                "name": name,
+                "bagtype": bagtype.data["id"],
+                "quantity": row["quantity"],
+                "part": get_part_by_name(parts, row["part"]),
             },
         )
 
