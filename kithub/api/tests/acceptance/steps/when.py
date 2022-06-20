baf254ac085fda7083a1ce06c4a6615ad1c4be3e
David ONeill <mrdavidoneillll@gmail.com>
2022-06-20 10:50:54 +0100

    Add create model condtion part or not Add update bag

--- kithub/api/tests/acceptance/steps/when.py
+++ kithub/api/tests/acceptance/steps/when.py
@@ -2,6 +2,7 @@ import json
 from behave import when
 from django.urls import reverse
 from django.core.serializers.json import DjangoJSONEncoder
+from utils import get_bagtype_by_kind
 
 
 @when('I run "python manage.py behave"')
@@ -11,17 +12,38 @@ def run_command(context):
 
 @when('I create a "{model}" called "{name}"')
 def create_model(context, model, name):
-    context.response = context.test.client.post(
-        reverse(f"{model}-list"),
-        {
-            "name": name,
-            "description": f"A description of {name}",
-        },
-    )
+
+    if model == "part":
+
+        payload = {"name": name, "description": f"A description of {name}"}
+    else:
+        payload = {"kind": name}
+
+    context.response = context.test.client.post(reverse(f"{model}-list"), payload)
+
+
+@when('I create "{quantity}" "{model}" of "{bagtype}" kind')
+def create_model(context, quantity, model, bagtype):
+
+    bagtypes = context.test.client.get(reverse("bagtype-list")).data
+
+    if model == "bag":
+        payload = {
+            "name": "",
+            "quantity": int(quantity),
+            "complete": False,
+            "kind": get_bagtype_by_kind(bagtypes, bagtype)["id"],
+        }
+    else:
+        payload = {"kind": name}
+
+    context.response = context.test.client.post(reverse(f"{model}-list"), payload)
 
 
 @when('I read the first "{model}"')
 def read_first_model(context, model):
+    context.response = context.test.client.get(reverse(f"{model}-list"))
+
     context.response = context.test.client.get(
         reverse(f"{model}-detail", args=(context.response.data[0]["id"],))
     )
@@ -63,8 +85,34 @@ def update_part(context):
         )
 
 
+@when("I update the first bag as follows")
+def update_bag(context):
+    bagtypes = context.test.client.get(reverse("bagtype-list")).data
+    context.response = context.test.client.get(reverse("bag-list"))
+
+    for row in context.table:
+        data = {
+            "name": row["name"],
+            "kind": get_bagtype_by_kind(bagtypes, row["kind"])["id"],
+            "complete": bool(row["complete"]),
+            "quantity": int(row["quantity"]),
+        }
+        context.response = context.test.client.put(
+            reverse(
+                "bag-detail",
+                kwargs={
+                    "pk": context.response.data[0]["id"],
+                },
+            ),
+            data=json.dumps(data, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+
+
 @when('I delete the first "{model}"')
 def delete_first_model(context, model):
+    context.response = context.test.client.get(reverse(f"{model}-list"))
+
     context.response = context.test.client.delete(
         reverse(f"{model}-detail", args=(context.response.data[0]["id"],))
     )
