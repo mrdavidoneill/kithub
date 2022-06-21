c34d98f591a6da7ade77d65e626564560e51c604
David ONeill <mrdavidoneillll@gmail.com>
2022-06-21 12:25:46 +0100

    Add see K, V of first and last purchase

--- kithub/api/tests/acceptance/steps/then.py
+++ kithub/api/tests/acceptance/steps/then.py
@@ -1,5 +1,6 @@
 from behave import register_type, then
 import parse
+import decimal
 from django.urls import reverse
 from utils import get_item_by_key
 
@@ -80,6 +81,28 @@ def see_numeric_key_value_of_last(context, key, value):
     context.test.assertEqual(response.data[-1][key], value)
 
 
+@then('the first purchase should have "{key}" equal to "{value}"')
+def see_key_value_of_first_purchase(context, key, value):
+    response = context.response
+    print(response.data)
+    actual = response.data["purchases"][0][key]
+    if key == "price":
+        value = decimal.Decimal(value)
+        actual = decimal.Decimal(actual)
+    context.test.assertEqual(actual, value)
+
+
+@then('the last purchase should have "{key}" equal to "{value}"')
+def see_key_value_of_last_purchase(context, key, value):
+    response = context.response
+    print(response.data)
+    actual = response.data["purchases"][-1][key]
+    if key == "price":
+        value = decimal.Decimal(value)
+        actual = decimal.Decimal(actual)
+    context.test.assertEqual(actual, value)
+
+
 @then('the status code should be "{code:Number}"')
 def status_code_check(context, code):
     response = context.response
@@ -102,3 +125,14 @@ def get_need_to_buy(context, item, model, quantity, missing):
     print(f"item {item}")
     parts_to_buy = item["parts_to_buy"]
     context.test.assertEqual(parts_to_buy[missing], quantity)
+
+
+@then('I should see "{letter}" is "{quantity:Number}" of part "{part_name}"')
+def check_part_in_ingredient(context, letter, quantity, part_name):
+    ingredients = context.response.data["ingredients"]
+    ingredient = get_item_by_key(ingredients, letter, "name")
+    parts = context.test.client.get(reverse("part-list")).data
+    part = get_item_by_key(parts, part_name, "name")
+
+    context.test.assertEqual(ingredient["part"], part["id"])
+    context.test.assertEqual(ingredient["quantity"], quantity)
