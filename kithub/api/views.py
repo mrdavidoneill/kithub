b42f5837b1500eceb4f3dcb22fc2021327cf55b3
David ONeill <mrdavidoneillll@gmail.com>
2022-06-22 09:23:28 +0200

    Format

--- kithub/api/views.py
+++ kithub/api/views.py
@@ -147,7 +147,8 @@ def parts_to_buy_for_bagtype(bagtype, quantity):
     # Iterate through each needed part
     for part in parts_needed:
         required_quantity = (
-            BagIngredient.objects.filter(bagtype=bagtype, part=part).first().quantity
+            BagIngredient.objects.filter(
+                bagtype=bagtype, part=part).first().quantity
             * quantity
         )
         # Calculate how short the stocked quantity is (pos if short, neg if OK)
@@ -205,7 +206,8 @@ def parts_to_buy_for_kittype(kittype, quantity):
 
             # If already needing to buy stock, increment amount
             if part.name in parts_to_buy:
-                parts_to_buy[part.name] += num_of_bags_needed * required_quantity
+                parts_to_buy[part.name] += num_of_bags_needed * \
+                    required_quantity
             # If not enough parts to fulfil request
             else:
                 parts_to_buy[part.name] = quantity_to_buy
