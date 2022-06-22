1661beca5bf33f795d083d973f32622066a93c56
David ONeill <mrdavidoneillll@gmail.com>
2022-06-22 09:23:51 +0200

    Add comments

--- kithub/urls.py
+++ kithub/urls.py
@@ -6,26 +6,31 @@ from .api import views
 urlpatterns = [
     # Automatic API URL routing
     path("", include(router.urls)),
+    # Web browsable admin API
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("dividebag/", views.dividebag, name="dividebag"),
-    path("dividekit/", views.dividekit, name="dividekit"),
+    # Endpoints for UC25-UC31
+    path("dividebag/", views.dividebag, name="dividebag"),  # UC25
+    path("dividekit/", views.dividekit, name="dividekit"),  # UC26
     path("potentialkits/<int:kittype>/",
-         views.potentialkits, name="potentialkits"),
-    path("potentialkits/", views.all_potentialkits, name="allpotentialkits"),
+         views.potentialkits, name="potentialkits"),  # UC27
+    path("potentialkits/", views.all_potentialkits,
+         name="allpotentialkits"),  # UC27
     path("potentialbags/<int:bagtype>",
-         views.potentialbags, name="potentialbags"),
-    path("potentialbags/", views.all_potentialbags, name="allpotentialbags"),
+         views.potentialbags, name="potentialbags"),  # UC28
+    path("potentialbags/", views.all_potentialbags,
+         name="allpotentialbags"),  # UC28
     path(
         "partstobuyforkit/<int:kittype>/<int:quantity>",
         views.partstobuyforkit,
         name="partstobuyforkit",
-    ),
+    ),  # UC29
     path(
         "partstobuyforbag/<int:bagtype>/<int:quantity>",
         views.partstobuyforbag,
         name="partstobuyforbag",
-    ),
+    ),  # UC30
     path("unfinishedbag/<int:bagtype>",
-         views.unfinishedbag, name="unfinishedbag"),
-    path("unfinishedbag/", views.all_unfinishedbags, name="allunfinishedbags"),
+         views.unfinishedbag, name="unfinishedbag"),  # UC31
+    path("unfinishedbag/", views.all_unfinishedbags,
+         name="allunfinishedbags"),  # UC31
 ]
