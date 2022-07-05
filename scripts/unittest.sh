0120055e22ce34fbfc0cc81099fb098aadc2ea52
David ONeill <mrdavidoneillll@gmail.com>
2022-07-05 17:32:30 +0100

    Activate virtual env in workspace

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -euo pipefail
+
+# Get the unique venv folder using *inside* workspace
+VENV=".venv-$BUILD_NUMBER"
+
+# Activate virtual environment
+source "$VENV/bin/activate"
 
 ./manage.py test kithub.api.tests.unit
