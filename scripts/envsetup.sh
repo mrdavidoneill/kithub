5a00fa238767cefafbf575b6c3920388e892bbc8
David ONeill <mrdavidoneillll@gmail.com>
2022-07-05 16:30:52 +0100

    Remove logs Install virtualenv

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
+python3 -m pip install --user virtualenv
+
 # Get an unique venv folder to using *inside* workspace
 VENV=".venv-$BUILD_NUMBER"
 
 # Initialize new venv
-virtualenv "$VENV"
+python3 -m virtualenv "$VENV"
 
 # Update pip
 PS1="${PS1:-}" source "$VENV/bin/activate"
 
 #!/bin/sh
 pip install -r requirements.txt
-
-if [ -d "logs" ] 
-then
-    echo "Log folder exists." 
-else
-    mkdir logs
-    touch logs/error.log logs/access.log
-fi
-
-sudo chmod -R 777 logs
