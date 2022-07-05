1a403feb28d862b01695f0b6aad1064721438afd
David ONeill <mrdavidoneillll@gmail.com>
2022-07-05 17:32:49 +0100

    Remove pip update

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -7,8 +7,8 @@ VENV=".venv-$BUILD_NUMBER"
 # Initialize new venv
 python3 -m venv "$VENV"
 
-# Update pip
-PS1="${PS1:-}" source "$VENV/bin/activate"
+# Activate virtual environment
+source "$VENV/bin/activate"
 
 #!/bin/sh
 pip install -r requirements.txt
